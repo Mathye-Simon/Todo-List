14c192eb5f60b8c652bcec19518726e7790b587f
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-10 21:38:15 +0200

    empty input error handling

--- assets/dom.js
+++ assets/dom.js
@@ -1,16 +1,19 @@
 // accessing html elements
 const addBtn = document.querySelector('#addBtn')
+const removeBtn = document.querySelector("#removeBtn")
 const todoListContainer = document.querySelector('#todoListContainer')
 const todoContent = document.querySelector('#todoContent')
-// creating a paragraph element
 
-
-
+// add todo with a checkbox
 addBtn.addEventListener('click',
+    
     function () {
-        let todo = todoContent.value
+        let todoText = todoContent.value
+        if (todoText == '') {
+            console.log('error: no input provided');
+    } else {
         const paragraph = document.createElement('p')
-        paragraph.textContent = todo
-        todoListContainer.append(paragraph)
+        paragraph.textContent = todoText
+        todoListContainer.append(paragraph)}
     }
 )
