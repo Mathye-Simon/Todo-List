ada6013bdf60f63b4975a062cefd81cd7acb708d
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-10 21:52:46 +0200

    each todo has a unique id

--- assets/dom.js
+++ assets/dom.js
@@ -3,17 +3,22 @@ const addBtn = document.querySelector('#addBtn')
 const removeBtn = document.querySelector("#removeBtn")
 const todoListContainer = document.querySelector('#todoListContainer')
 const todoContent = document.querySelector('#todoContent')
-
+// to make unique todos
+todoNumber = 0;
 // add todo with a checkbox
 addBtn.addEventListener('click',
     
     function () {
+        
         let todoText = todoContent.value
+        // error handling
         if (todoText == '') {
             console.log('error: no input provided');
     } else {
         const paragraph = document.createElement('p')
         paragraph.textContent = todoText
+        // set id for each todo
+        paragraph.setAttribute('id',`todo ${todoNumber+=1}`)
         todoListContainer.append(paragraph)}
     }
 )
