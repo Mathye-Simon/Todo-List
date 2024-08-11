da0d79c2f480384785f295d3fd6160ca2ec8c2a4
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-11 05:55:13 +0200

    improved how user adds a todo

--- assets/dom.js
+++ assets/dom.js
@@ -2,23 +2,36 @@
 const addBtn = document.querySelector('#addBtn')
 const removeBtn = document.querySelector("#removeBtn")
 const todoListContainer = document.querySelector('#todoListContainer')
-const todoContent = document.querySelector('#todoContent')
+
+
 // to make unique todos
 todoNumber = 0;
 // add todo with a checkbox
 addBtn.addEventListener('click',
     
     function () {
-        
-        let todoText = todoContent.value
+    const todoText = document.querySelector('#todoContent').value   
         // error handling
-        if (todoText == '') {
+    if (todoText == '') {
             console.log('error: no input provided');
     } else {
-        const paragraph = document.createElement('p')
-        paragraph.textContent = todoText
-        // set id for each todo
-        paragraph.setAttribute('id',`todo ${todoNumber+=1}`)
-        todoListContainer.append(paragraph)}
+        // new elements each click
+        const todoCheckbox = document.createElement('input')
+        todoCheckbox.setAttribute('type','checkbox')
+        const todoDiv = document.createElement('div')
+        const list = document.createElement('li')
+        list.textContent = todoText
+        // set id for each element added
+        list.setAttribute('id',`todo ${todoNumber+=1}`)
+        todoDiv.setAttribute('id',`todoDiv ${todoNumber}`)
+        todoCheckbox.setAttribute('id',`todoCheckbox ${todoNumber}`)
+
+        // set class for styling
+        todoDiv.setAttribute('class',`todoDiv`)
+
+
+        // structuring elements
+        todoDiv.append(todoCheckbox,list)
+        todoListContainer.append(todoDiv)}
     }
 )
