3c73e77be376ea14baf8e4f513536fd8e9f5c710
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-11 06:31:56 +0200

    addded an event handler

--- assets/dom.js
+++ assets/dom.js
@@ -2,36 +2,42 @@
 const addBtn = document.querySelector('#addBtn')
 const removeBtn = document.querySelector("#removeBtn")
 const todoListContainer = document.querySelector('#todoListContainer')
+const todoInput = document.querySelector('#todoContent')
 
 
 // to make unique todos
 todoNumber = 0;
-// add todo with a checkbox
-addBtn.addEventListener('click',
-    
-    function () {
-    const todoText = document.querySelector('#todoContent').value   
+// add todo with a checkbox based on an event
+function handleEvent (event) {
+    if (event.type == 'click' || event.type == 'keydown' && event.key == 'Enter') {
+
+        const todoText = todoInput.value   
         // error handling
-    if (todoText == '') {
+        if (todoText == '') {
             console.log('error: no input provided');
-    } else {
+        } else {
         // new elements each click
-        const todoCheckbox = document.createElement('input')
-        todoCheckbox.setAttribute('type','checkbox')
-        const todoDiv = document.createElement('div')
-        const list = document.createElement('li')
-        list.textContent = todoText
-        // set id for each element added
-        list.setAttribute('id',`todo ${todoNumber+=1}`)
-        todoDiv.setAttribute('id',`todoDiv ${todoNumber}`)
-        todoCheckbox.setAttribute('id',`todoCheckbox ${todoNumber}`)
+            const todoCheckbox = document.createElement('input')
+            todoCheckbox.setAttribute('type','checkbox')
+            const todoDiv = document.createElement('div')
+            const list = document.createElement('li')
+            list.textContent = todoText
+            // set id for each element added
+            list.setAttribute('id',`todo ${todoNumber+=1}`)
+            todoDiv.setAttribute('id',`todoDiv ${todoNumber}`)
+            todoCheckbox.setAttribute('id',`todoCheckbox ${todoNumber}`)
 
-        // set class for styling
-        todoDiv.setAttribute('class',`todoDiv`)
+            // set class for styling
+            todoDiv.setAttribute('class',`todoDiv`)
 
 
-        // structuring elements
-        todoDiv.append(todoCheckbox,list)
-        todoListContainer.append(todoDiv)}
+            // structuring elements
+            todoDiv.append(todoCheckbox,list)
+            todoListContainer.append(todoDiv)}
     }
-)
+}
+addBtn.addEventListener('click',handleEvent)
+todoInput.addEventListener('keydown', handleEvent)
+
+
+
